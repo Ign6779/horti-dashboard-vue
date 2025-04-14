60b2d806f2ccaaf32103e4e098c2af594c5a70e1
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2025-04-14 11:22:26 +0200

    router added

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="p-4">
+      <h1 class="text-2xl font-bold">Home Page</h1>
+      <p>Welcome to the Horti Dashboard 👋</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
